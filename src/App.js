2fcd088900ec0fb4b7a596b89a48e3439090009f
Headgie <headgy@yandex.ru>
2020-02-26 01:27:43 +0300

    player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
-import { MediaPlayer } from '@cassette/player';
-import '@cassette/player/dist/css/cassette-player.css';
+import { MediaPlayerControls } from "@cassette/player";
+import { PlayerContextProvider, playerContextFilter } from "@cassette/core";
+import { MediaProgressBar } from '@cassette/components';
 
-
+import "@cassette/player/dist/css/cassette-player.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
 class App extends React.Component {
   constructor() {
@@ -74,22 +77,80 @@ class App extends React.Component {
     };
   }
   render() {
-    return <div className="App">
-    <div className="header">Header</div>
-    
-    <MediaPlayer playlist={this.state.songs}
-    controls={[
-      'spacer','backskip',
-      'playpause',
-      'forwardskip',
-      'mute',
-     
-      'spacer',
-      'progress'
-    ]}  />
-    
-    </div>;
+    return (
+      <div className="App">
+        <div className="header">Header</div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <div>
+          <PlayerContextProvider playlist={this.state.songs}>
+            <MediaPlayerControls
+              controls={[
+                "spacer",
+                "backskip",
+                "playpause",
+                "forwardskip",
+                "mute",
+                "spacer",
+                "progress"
+              ]}
+            ></MediaPlayerControls>
+            <div>
+              <h3>Select a track:</h3>
+              <PlaylistMenu />
+            </div>
+          </PlayerContextProvider>
+        </div>
+      </div>
+    );
   }
 }
+
+
+function PlaylistMenu({ playlist, activeTrackIndex, onSelectTrackIndex }) {
+  return (
+    <ol>
+      {playlist.map((track, i) => {
+        const playing = activeTrackIndex === i;
+        return (
+          <li key={track.title}>
+            {playing && <div>
+              <strong>{track.title} (playing)</strong>
+              <MediaProgressBar
+                  progressDirection="right"
+                  style={{ background: 'black', height: 30 }}
+                  progressStyle={{ background: 'orange' }}
+                  handle={
+                    <div
+                      style={{
+                        width: 30,
+                        height: 30,
+                        background: '#fff',
+                        borderRadius: '10%',
+                        border: '2px solid black'
+                      }}
+                    />
+                  }
+                />
+            </div>}
+            {!playing &&
+              <button
+                onClick={() => onSelectTrackIndex(i)}
+              >
+                {track.title}
+              </button>}
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
+
+PlaylistMenu = playerContextFilter(PlaylistMenu, [
+  'playlist',
+  'activeTrackIndex',
+  'onSelectTrackIndex'
+]);
 
 export default App;
